bbf6182bbbc52bd7b47dc25140cc34101fe8a860
Ruben Mennes <ruben.mennes@me.com>
2024-03-01 14:26:50 +0100

    Remove invalid test

--- internal/resource_provider/service_test.go
+++ internal/resource_provider/service_test.go
@@ -12,13 +12,11 @@ import (
 	"github.com/raito-io/bexpression/utils"
 	"github.com/raito-io/cli/base/resource_provider"
 	"github.com/raito-io/golang-set/set"
-	"github.com/raito-io/sdk-go"
 	"github.com/raito-io/sdk-go/services"
 	sdkTypes "github.com/raito-io/sdk-go/types"
 	"github.com/raito-io/sdk-go/types/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/require"
 
 	"cli-plugin-dbt/internal/manifest"
 )
@@ -674,20 +672,4 @@ func createDbtService(t *testing.T, dataSourceId string) (*DbtService, *MockAcce
 	service := NewDbtService(&resource_provider.UpdateResourceInput{DataSourceId: dataSourceId}, apMock, manifestParser, logger)
 
 	return service, apMock
-}
-
-func TestDbtService_IT(t *testing.T) {
-	//t.Skip()
-
-	ctx := context.Background()
-
-	client := sdk.NewClient(ctx, "demo", "ci+sync@raito.io", "7yigBDRJaidp9qlfR83mzayD!", sdk.WithUrlOverride("https://api.raito.dev"))
-
-	service := NewDbtService(&resource_provider.UpdateResourceInput{
-		DataSourceId: "Bp14M1b0rJNqQABg4-Tvr",
-	}, client.AccessProvider(), manifest.NewManifestParser(), hclog.NewNullLogger())
-
-	_, _, _, _, err := service.RunDbt(ctx, "/Users/rubenmennes/git/testing/dbt/dbt_bq_demo/target/manifest.json")
-
-	require.NoError(t, err)
 }
